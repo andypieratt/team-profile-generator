7afe691e3d2b0ee5352dc50c800d8c2e38a45a53
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-05-31 18:06:55 -0600

    added initial files that I will need to complete this challenge and I began working on the classes and general layout to complete the command line prompts for the team

---
+++ index.js
@@ -0,0 +1,5 @@
+//REQUIRED VARIABLES
+const inquirer = require("inquirer");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
