088303ce6bff2b58694c6846757b0cfaa3f9b304
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-06-02 17:04:30 -0600

    tweaked the lib/ classes and I think I have them correct now, and added my html template to the index.js file that it will write the data to. Still need to work on how to store the users inputs and dynamically generate cards in the html file

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 //REQUIRED VARIABLES
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 //Inquirer
 function managerPrompts() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the team manager's name?",
-      name: "managerName",
-    },
-    {
-      type: "input",
-      message: "What is the manager's ID?",
-      name: "managerId",
-    },
-    {
-      type: "input",
-      message: "What is the manager's email address?",
-      name: "managerEmail",
-    },
-    {
-      type: "input",
-      message: "What is the manager's office number?",
-      name: "officeNum",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "managerName",
+      },
+      {
+        type: "input",
+        message: "What is the manager's ID?",
+        name: "managerId",
+      },
+      {
+        type: "input",
+        message: "What is the manager's email address?",
+        name: "managerEmail",
+      },
+      {
+        type: "input",
+        message: "What is the manager's office number?",
+        name: "officeNum",
+      },
+    ])
+    .then((response) => {
+      const manager = new Manager();
+      newMember();
+    });
 }
 
 function engineerPrompts() {
@@ -90,7 +99,7 @@ function newMember() {
     ])
     .then((response) => {
       if (response.nextMember === "Engineer") {
-        engineerPrompts();
+        engineerPrompts(); //WHY ISN'T THIS WORKING???
       } else if (response.nextMember === "Intern") {
         internPrompts();
       } else {
@@ -100,5 +109,56 @@ function newMember() {
 }
 
 function generateFile() {
-  fs.writeFile("./dist/index.html", ``);
+  fs.writeFile(
+    "./dist/index.html",
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--CUSTOM BOOTSTRAP CSS-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>My Team</title>
+</head>
+<body>
+  <!--HEADER SECTION-->
+  <header class="bg-danger p-3">
+    <h1 class="text-center text-white m-3">My Team:</h1>
+  </header>
+
+  <!--CARD/CONTENT SECTION-->
+  <section class="m-5">
+    <div class="card border border-danger d-flex align-content-center flex-wrap" style="width: 18rem;">
+      <!-- <img src="..." class="card-img-top" alt="..."> -->
+      <div class="card-body">
+        <h5 class="card-title">(EMPLOYEE TYPE PLACEHOLDER)</h5>
+        <p class="card-text">
+          (NAME PLACEHOLDER)
+        </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">(ID PLACEHOLDER)</li>
+        <li class="list-group-item">(EMAIL PLACEHOLDER)</li>
+        <li class="list-group-item">(GITHUB/SCHOOL/OFFICE PLACEHOLDER)</li>
+      </ul>
+      <!-- <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div> -->
+    </div>
+  </section>
+
+  <!--BOOTSTRAP JS IMPORT-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <!--CUSTOM JS-->
+  <script src="/index.js"></script>
+</body>
+</html>`
+  );
 }
+
+//CALLING FUNCTIONS
+managerPrompts();
